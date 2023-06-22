8bf38cd5afa663dd0d5f1ceb4e3add57805d5c1c
Amele9 <amele090044@gmail.com>
2023-06-22 22:47:25 +0500

    Updated example

--- examples/uploadFile/main.go
+++ examples/uploadFile/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"path"
 
 	"github.com/green-api/whatsapp-api-client-golang/pkg/api"
 )
@@ -13,10 +15,25 @@ func main() {
 		APITokenInstance: "d75b3a66374942c5b3c019c698abc2067e151558acbd412345",
 	}
 
-	response, err := GreenAPI.Methods().Sending().UploadFile("example.png")
+	uploadFileResponse, err := GreenAPI.Methods().Sending().UploadFile("example.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(response)
+	fmt.Println(uploadFileResponse)
+
+	urlFile := uploadFileResponse["urlFile"].(string)
+	request, _ := http.NewRequest("GET", urlFile, nil)
+	fileName := path.Base(request.URL.Path)
+
+	sendFileByUrlResponse, err := GreenAPI.Methods().Sending().SendFileByUrl(map[string]interface{}{
+		"chatId":   "11001234567@c.us",
+		"urlFile":  urlFile,
+		"fileName": fileName,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(sendFileByUrlResponse)
 }
