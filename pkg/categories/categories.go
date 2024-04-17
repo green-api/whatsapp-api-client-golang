ed3593f9442250d453aecf84acf1403d36b9ad8f
dephea <lwarayl@hotmail.com>
2024-04-17 07:45:46 +0300

    status methods

--- pkg/categories/categories.go
+++ pkg/categories/categories.go
@@ -6,38 +6,78 @@ type GreenAPICategories struct {
 	GreenAPI methods.GreenAPIInterface
 }
 
+// Account category presents methods for working with the account.
+// https://green-api.com/en/docs/api/account/
 func (c GreenAPICategories) Account() methods.AccountCategory {
 	return methods.AccountCategory{GreenAPI: c.GreenAPI}
 }
 
+// Device category presents methods for working with the device (phone).
+// https://green-api.com/en/docs/api/phone/
 func (c GreenAPICategories) Device() methods.DeviceCategory {
 	return methods.DeviceCategory{GreenAPI: c.GreenAPI}
 }
 
+// Groups category presents methods for working with group chats.
+// https://green-api.com/en/docs/api/groups/
 func (c GreenAPICategories) Groups() methods.GroupsCategory {
 	return methods.GroupsCategory{GreenAPI: c.GreenAPI}
 }
 
+// Journals present methods for working with incoming and outgoing messages.
+// https://green-api.com/en/docs/api/journals/
 func (c GreenAPICategories) Journals() methods.JournalsCategory {
 	return methods.JournalsCategory{GreenAPI: c.GreenAPI}
 }
 
+// Queues category presents methods for working with a messages queue.
+// https://green-api.com/en/docs/api/queues/
 func (c GreenAPICategories) Queues() methods.QueuesCategory {
 	return methods.QueuesCategory{GreenAPI: c.GreenAPI}
 }
 
+// ReadMark category presents methods for working with chat read mark.
+// https://green-api.com/en/docs/api/marks/
 func (c GreenAPICategories) ReadMark() methods.ReadMarkCategory {
 	return methods.ReadMarkCategory{GreenAPI: c.GreenAPI}
 }
 
+// Receiving category presents methods for working with receiving events.
+// https://green-api.com/en/docs/api/receiving/
 func (c GreenAPICategories) Receiving() methods.ReceivingCategory {
 	return methods.ReceivingCategory{GreenAPI: c.GreenAPI}
 }
 
+// Sending category presents methods for sending different messages.
+// https://green-api.com/en/docs/api/sending/
 func (c GreenAPICategories) Sending() methods.SendingCategory {
 	return methods.SendingCategory{GreenAPI: c.GreenAPI}
 }
 
+// Service category presents different service methods.
+// https://green-api.com/en/docs/api/service/
 func (c GreenAPICategories) Service() methods.ServiceCategory {
 	return methods.ServiceCategory{GreenAPI: c.GreenAPI}
+}
+
+// Partner category presents exclusive methods for partners.
+// The partnership scheme involves deeper integration with the service
+// and working with a larger number of instances on your side:
+//
+// * Instance management via API
+// * Postpaid billing system (starting from the second month of operation)
+// * Daily billing (for created and not deleted instances)
+// * Dedicated support line
+// For questions regarding connection to the partnership scheme
+// and additional conditions, please contact us via email
+// at support@green-api.com or via chat on the website.
+// https://green-api.com/en/docs/partners/
+func (c GreenAPICategories) Partner() methods.PartnerCategory {
+	return methods.PartnerCategory{GreenAPI: c.GreenAPI}
+}
+
+// Status category presents methods for statuses
+// https://green-api.com/en/docs/api/statuses/
+func (c GreenAPICategories) Status() methods.StatusCategory {
+	return methods.StatusCategory{GreenAPI: c.GreenAPI}
 }
--- pkg/categories/methods/journals.go
+++ pkg/categories/methods/journals.go
@@ -5,11 +5,13 @@ type JournalsCategory struct {
 }
 
 // GetChatHistory returns the chat message history.
+// https://green-api.com/en/docs/api/journals/GetChatHistory/
 func (c JournalsCategory) GetChatHistory(parameters map[string]interface{}) ([]interface{}, error) {
 	return c.GreenAPI.ArrayRequest("POST", "getChatHistory", parameters, "")
 }
 
 // GetMessage returns a chat message.
+// https://green-api.com/en/docs/api/journals/GetMessage/
 func (c JournalsCategory) GetMessage(chatId, idMessage string) (map[string]interface{}, error) {
 	return c.GreenAPI.Request("POST", "getMessage", map[string]interface{}{
 		"chatId":    chatId,
@@ -19,11 +21,13 @@ func (c JournalsCategory) GetMessage(chatId, idMessage string) (map[string]inter
 
 // LastIncomingMessages returns the most recent incoming messages
 // of the account.
+// https://green-api.com/en/docs/api/journals/LastIncomingMessages/
 func (c JournalsCategory) LastIncomingMessages(parameters map[string]interface{}) ([]interface{}, error) {
 	return c.GreenAPI.ArrayRequest("GET", "lastIncomingMessages", parameters, "")
 }
 
 // LastOutgoingMessages returns the last sent messages of the account.
+// https://green-api.com/en/docs/api/journals/LastOutgoingMessages/
 func (c JournalsCategory) LastOutgoingMessages(parameters map[string]interface{}) ([]interface{}, error) {
 	return c.GreenAPI.ArrayRequest("GET", "lastOutgoingMessages", parameters, "")
 }
